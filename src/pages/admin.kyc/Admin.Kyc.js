98ed9e278579c2ab451d66c5db3e7eb1bb324e5c
nnebuchi <nnebuchiosigbo340@gmail.com>
2026-06-12 12:59:27 +0100

    restored kyc approval buttons

--- src/pages/admin.kyc/Admin.Kyc.js
+++ src/pages/admin.kyc/Admin.Kyc.js
@@ -134,8 +134,8 @@ const Kyc = () => {
                     </div>
                   ) : (
                     <div style={{display: 'flex', alignItems: 'center', gap: '8px'}}>
-                      {/* Approved <FaCircle style={{ color: 'green' }} /> */}
-                       <button onClick={() => handleApprove(info)}>Approve</button>
+                      Approved <FaCircle style={{ color: 'green' }} />
+                       {/* <button onClick={() => handleApprove(info)}>Approve</button> */}
                       <button onClick={() => handleReject(info)} className="btn-danger">Revert</button>
                     </div>
                   )}
